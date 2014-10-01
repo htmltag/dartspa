67da4c45bd7b110d2cef62dd0849223c8cc70173
htmltag <jonathansoylandlier@gmail.com>
2014-10-02 00:35:19 +0200

    Just removed some comments

--- DartSPA/web/dartspa.dart
+++ DartSPA/web/dartspa.dart
@@ -9,8 +9,6 @@ part 'models.dart';
 void main() {
   var appContainer = document.querySelector("#dartspa");
   _app = new Application(appContainer);
-  //app.AddToDo(false, "Todo", "What to do?");
-  //app.AddToDo(true, "My Todo", "To do or not to do?");
   app.CreateUI();
   app.DefaultView();
 }
